cd5d9bc50857204d93686ff9dab59d1caf81e327
Crocou <128105185+Crocou@users.noreply.github.com>
2025-07-06 12:56:43 +0900

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -12,6 +12,7 @@ from google.oauth2.service_account import Credentials
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 def get_driver():
     service = None 
